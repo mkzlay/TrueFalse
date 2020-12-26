b2a9e0aa19e984fa70ae691e6ba17eb695d1656d
Mustafa Kaan Kızılay <75048064+mkzlay@users.noreply.github.com>
2020-12-27 01:51:02 +0300

    Add files via upload

---
+++ typetoconversion-truefalse.py
@@ -0,0 +1,22 @@
+# False, True terimlerinin matematiksel karşılığı:
+# False = 0, True = 1 ispatını tip dönüşümü ile birlikte yapalım.
+
+isOnline = True
+isOffline = False 
+
+isOnline = int(isOnline)
+
+print(isOnline)
+print(type(isOnline))
+
+
+isOffline = int(isOffline)
+
+print(isOffline)
+print(type(isOffline))
+
+
+print()
+print("KK")
+
+
